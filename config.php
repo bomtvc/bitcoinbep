e826dfe2c25dee420c9352ca5c13388b5c0e73a0
bomtvc <32728677+bomtvc@users.noreply.github.com>
2020-02-12 16:49:31 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?
 
-$UserAgent = "xxxxxxxxxx";
-$Cookie = "lt=xxxxxxxxx";
-$username = "xxxxxxxx";
-$id = "xxxxxxxx";
+$UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36";
+$Cookie = "lt=5e43c857f38ad%3A107e10775d61b837903cff8e98edaf32318bfd63923b158e8fed9ed81dc5eca89f";
+$username = "bomtvc";
+$id = "143929";
